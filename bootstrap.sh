ce89b0b41527d6730bb8b4259a4f970c08bc61cb
lcdse7en <2353442022@qq.com>
2022-08-28 16:14:09 +0800

    tmux

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+sleep 1
 tmux new-session -s server -d
-tmux send-keys -t server:0.0 "c" "C-m" "s" "C-m" "server" "C-m"
+tmux send-keys -t server:1.1 "c" "C-m" "s" "C-m"
+sleep 1
+tmux send-keys "server" "C-m"
 tmux split-window -h -p 50
 tmux send-keys "c" "C-m"
